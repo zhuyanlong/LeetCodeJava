31d633f3b54670974d7aefd3531378ed965c39ec
zhuyanlongcs@hotmail.com <ZYL@19960201zyl>
2021-03-30 12:47:21 +0800

    树的中序和后序遍历

--- src/com/company/PostorderTraversal.java
+++ src/com/company/PostorderTraversal.java
@@ -1,15 +1,24 @@
 package com.company;
 
+import sun.reflect.generics.tree.Tree;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 public class PostorderTraversal {
     public static void main(String args[]){
         TreeNode root=new TreeNode(1);
-        root.right=new TreeNode(2);
-        root.right.left=new TreeNode(3);
+        root.left=new TreeNode(2);
+        root.right=new TreeNode(3);
+        root.left.right=new TreeNode(5);
+        root.right.left=new TreeNode(6);
         List<Integer> res=new ArrayList<Integer>();
         recursion(root,res);
+        for(int i=0;i<res.size();i++){
+            System.out.print(res.get(i)+" ");
+        }
     }
 
     public static void recursion(TreeNode root,List<Integer> res){
@@ -23,5 +32,25 @@ public class PostorderTraversal {
             recursion(root.right,res);
         }
         res.add(root.val);
+    }
+
+//    由先序遍历改造成的后序遍历
+    public static void reversepreorder(TreeNode root,List<Integer> res){
+        if(root==null){
+            return;
+        }
+        Stack<TreeNode> stack=new Stack<>();
+        stack.add(root);
+        while(!stack.isEmpty()){
+            TreeNode tmp=stack.pop();
+            res.add(tmp.val);
+            if(tmp.left!=null){
+                stack.add(tmp.left);
+            }
+            if(tmp.right!=null){
+                stack.add(tmp.right);
+            }
+        }
+        Collections.reverse(res);
     }
 }
--- src/com/company/_94BinaryTreeInorderTraversal.java
+++ src/com/company/_94BinaryTreeInorderTraversal.java
@@ -1,46 +1,23 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class _94BinaryTreeInorderTraversal {
     public static void main(String args[]){
         TreeNode root=new TreeNode(1);
-        root.right=new TreeNode(2);
-        root.right.left=new TreeNode(3);
+        root.left=new TreeNode(2);
+        root.right=new TreeNode(3);
+        root.left.right=new TreeNode(5);
+        root.right.left=new TreeNode(6);
         _94Solution sol=new _94Solution();
         sol.inorderTraversal(root);
     }
 }
 
-//使用栈的方式做中序遍历
-//class _94Solution {
-//    public List<Integer> inorderTraversal(TreeNode root) {
-//        List<Integer> res=new ArrayList<Integer>();
-//        List<TreeNode> stack=new ArrayList<TreeNode>();
-//        stack.add(root);
-//        while(stack.size()!=0){
-//            TreeNode tmp=stack.get(stack.size()-1);
-//            stack.remove(stack.size()-1);
-//            if(tmp.left!=null){
-//                stack.add(tmp.left);
-//            }
-//            if(tmp.right!=null){
-//                stack.add(tmp.right);
-//            }
-//
-//        }
-//        for(int i=0;i<res.size();i++){
-//            System.out.print(res.get(i)+" ");
-//        }
-//        return res;
-//    }
-//}
-
 class _94Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res=new ArrayList<Integer>();
-        traversal(root,res);
+        stacktraversal(root,res);
         for(int i=0;i<res.size();i++){
             System.out.print(res.get(i)+" ");
         }
@@ -57,6 +34,29 @@ class _94Solution {
         res.add(root.val);
         if(root.right!=null){
             traversal(root.right,res);
+        }
+    }
+
+//    模拟遍历过程，对于已经遍历过的节点，则不再遍历，避免循环
+    public void stacktraversal(TreeNode root,List<Integer> res){
+        if(root==null){
+            return;
+        }
+        Stack<TreeNode> stack=new Stack<>();
+        stack.add(root);
+        Set<TreeNode> visited=new HashSet<>();
+        while(!stack.isEmpty()){
+            TreeNode p=stack.peek();
+            while(p.left!=null && !visited.contains(p.left)){
+                stack.add(p.left);
+                p=p.left;
+            }
+            TreeNode tmp=stack.pop();
+            visited.add(tmp);
+            res.add(tmp.val);
+            if(tmp.right!=null){
+                stack.add(tmp.right);
+            }
         }
     }
 }
