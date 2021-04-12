ffd2b10d28dc4924d9f7cc307fb9f73a1df13ddc
yanlong <zhuyanlongcs@163.com>
2021-04-13 00:52:31 +0800

    add 8

---
+++ src/com/company/_8StringtoInteger.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class _8StringtoInteger {
+    public static void main(String args[]){
+
+    }
+    public static int myAtoi(String s) {
+        return 1;
+    }
+}
