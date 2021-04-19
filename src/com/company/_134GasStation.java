00581ee073485aaa09872bf93bd6de5026838d12
zhuyanlongcs@hotmail.com <ZYL@19960201zyl>
2021-04-19 11:59:03 +0800

    134

---
+++ src/com/company/_134GasStation.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class _134GasStation {
+    public static void main(String args[]){
+        int[] gas={2,3,4};
+        int[] cost={3,4,3};
+        int res=canCompleteCircuit(gas,cost);
+        System.out.println(res);
+    }
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        int start=0;
+        int total=0;
+        int sum=0;
+        for(int i=0;i<gas.length;i++){
+            total+=gas[i]-cost[i];
+            if(total<0){
+                total=0;
+                start=i+1;
+            }
+            sum+=gas[i]-cost[i];
+        }
+        if(sum<0){
+            return -1;
+        }
+        return start;
+    }
+}
